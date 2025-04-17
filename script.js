b1e960d05f735fb9f986b21ff99f820b69641a95
kartikeya06-coder <guptakartikeya06@gmail.com>
2025-04-17 17:25:22 +0530

    script.js

--- script.js
+++ script.js
@@ -34,18 +34,11 @@ let stopExecution = false;
 let currAudio = null;
 
 async function playSong(i) {
-    let api = await fetch("https://kartikeya06-coder.github.io/spotify-clone/Songs/");
-    // let data = await api.json();
-    let data = await api.text();
-    // console.log(data);
-    let div = document.createElement("div");
-    div.innerHTML = data;
-    // let link = div.querySelectorAll("a")[1].href;
-    // let aud = new Audio(link)
-    // aud.play();
 
-
-    let link = div.querySelectorAll("a")[i + 1].href;
+    const songs = ["https://kartikeya06-coder.github.io/spotify-clone/Songs/1_Beautiful_people.mp3" , "https://kartikeya06-coder.github.io/spotify-clone/Songs/2_I_ain't_worried.mp3" , "https://kartikeya06-coder.github.io/spotify-clone/Songs/3_Hold_my_hand.mp3" , "https://kartikeya06-coder.github.io/spotify-clone/Songs/4_APT.mp3" , "https://kartikeya06-coder.github.io/spotify-clone/Songs/5_Espresso.mp3" , "https://kartikeya06-coder.github.io/spotify-clone/Songs/6_Galwawy_girl.mp3" , "https://kartikeya06-coder.github.io/spotify-clone/Songs/7_Shape_of_you.mp3" , "https://kartikeya06-coder.github.io/spotify-clone/Songs/8_Azizam.mp3" , "https://kartikeya06-coder.github.io/spotify-clone/Songs/90_Try_everything.mp3" , "https://kartikeya06-coder.github.io/spotify-clone/Songs/91_Taste.mp3" , "https://kartikeya06-coder.github.io/spotify-clone/Songs/92_Call_when_you_break_up.mp3https://kartikeya06-coder.github.io/spotify-clone/Songs/92_Call_when_you_break_up.mp3" , "https://kartikeya06-coder.github.io/spotify-clone/Songs/93_I_can't_get_enough.mp3"];
+    
+    // let link = div.querySelectorAll("a")[i + 1].href;
+    let link = songs[i];
     currSong = div.querySelectorAll("a")[i + 1].innerText.slice(0, -4);
     // console.log(currSong);
 
@@ -105,52 +98,52 @@ async function main() {
 
     document.getElementsByClassName("card")[0].addEventListener("click", async () => {
         await playSong(0);
-        console.log(currSong);
+        // console.log(currSong);
 
     })
     document.getElementsByClassName("card")[1].addEventListener("click", async () => {
         await playSong(1);
-        console.log(currSong);
+        // console.log(currSong);
     })
     document.getElementsByClassName("card")[2].addEventListener("click", async () => {
         await playSong(2);
-        console.log(currSong);
+        // console.log(currSong);
     })
     document.getElementsByClassName("card")[3].addEventListener("click", async () => {
         await playSong(3);
-        console.log(currSong);
+        // console.log(currSong);
     })
     document.getElementsByClassName("card")[4].addEventListener("click", async () => {
         await playSong(4);
-        console.log(currSong);
+        // console.log(currSong);
     })
     document.getElementsByClassName("card")[5].addEventListener("click", async () => {
         await playSong(5);
-        console.log(currSong);
+        // console.log(currSong);
     })
     document.getElementsByClassName("card")[6].addEventListener("click", async () => {
         await playSong(6);
-        console.log(currSong);
+        // console.log(currSong);
     })
     document.getElementsByClassName("card")[7].addEventListener("click", async () => {
         await playSong(7);
-        console.log(currSong);
+        // console.log(currSong);
     })
     document.getElementsByClassName("card")[8].addEventListener("click", async () => {
         await playSong(8);
-        console.log(currSong);
+        // console.log(currSong);
     })
     document.getElementsByClassName("card")[9].addEventListener("click", async () => {
         await playSong(9);
-        console.log(currSong);
+        // console.log(currSong);
     })
     document.getElementsByClassName("card")[10].addEventListener("click", async () => {
         await playSong(10);
-        console.log(currSong);
+        // console.log(currSong);
     })
     document.getElementsByClassName("card")[11].addEventListener("click", async () => {
         await playSong(11);
-        console.log(currSong);
+        // console.log(currSong);
     })
 
 
